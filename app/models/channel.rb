3a74b9a2bf6aec68ff3f4f09c4e434c27a993560
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-05-31 16:06:03 -0300

    Generate models

---
+++ app/models/channel.rb
@@ -0,0 +1,8 @@
+class Channel < ApplicationRecord
+  has_many :messages, as: :messagable, dependent: :destroy
+  belongs_to :team
+  belongs_to :user
+
+  validates_presence_of :slug
+  validates :slug, uniqueness: true, format: { with: /\A[a-zA-Z0-9]+\Z/ }
+end
---
+++ app/models/talk.rb
@@ -0,0 +1,6 @@
+class Talk < ApplicationRecord
+  belongs_to :team
+  belongs_to :user_one, class_name: :User
+  belongs_to :user_two, class_name: :User
+  has_many :messages, as: :messagable, dependent: :destroy
+end
