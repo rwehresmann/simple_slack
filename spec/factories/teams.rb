1d7534c1937b96777bc8fbe1ea5adcc5121e9983
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-06-03 19:00:18 -0300

    Implemented factories

---
+++ spec/factories/teams.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+ factory :team do
+   slug "team"
+   user
+ end
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+ factory :user do
+   name "name"
+   email "user@mail.com"
+   password "secret123"
+ end
+end
