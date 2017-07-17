9ff4d0e7b68026db4e8a95a1f77b516c328cd8cc
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-07-17 12:55:21 -0300

    Configured cancancan rules

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,13 @@ class ApplicationController < ActionController::Base
 
   include CanCan::ControllerAdditions
 
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { head :forbidden, content_type: 'text/html' }
+      format.html { redirect_to main_app.root_url, notice: exception.message }
+    end
+  end
+
   protected
 
   def configure_permitted_parameters
