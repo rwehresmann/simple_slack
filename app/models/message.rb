33ccaed3aa93b7dcc1e1c31bad603c3b22b5b2d8
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-07-23 20:10:50 -0300

    Configured and implemented everithing related to ActionCable

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,7 @@ class Message < ApplicationRecord
   belongs_to :messagable, polymorphic: true
   belongs_to :user
 
-  validates_presence_of :body
+  validates_presence_of :body, :user
+
+  after_create_commit { MessageBroadcastJob.perform_later(self) }
 end
