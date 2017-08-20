26eba6497fc5711669cc8c9bd89f37e587496fa1
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-08-19 23:12:19 -0300

    Add feature tests of send messages

--- spec/feature/send_a_private_message_spec.rb
+++ spec/feature/send_a_private_message_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
-describe "Send a message to a channel", type: :feature, js: true do
+describe "Send a private message", type: :feature, js: true do
   it "shows messages sended in channel" do
-    user = create(:user)
-    team = create(:team, users: [user])
+    users = create_pair(:user)
+    team = create(:team, users: users)
     message_text = "Test message"
 
-    login_as user
+    login_as users[0]
     visit team_chat_path(team.slug)
+    find("#channel_users").click
+    find("#user_#{users[1].id}").click
     fill_in "new_message", with: message_text
     find("#new_message").native.send_keys :enter
 
