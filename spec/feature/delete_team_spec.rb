f998e2ffbe904103c6064190114dcf4a3880ee9b
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-08-20 00:34:11 -0300

    Add test for delete team feature

---
+++ spec/feature/delete_team_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "Delete a team", type: :feature, js: true do
+  it "shows an error message" do
+    user = create(:user)
+    team = create(:team, user: user)
+
+    login_as user
+    visit team_chat_path(team.slug)
+    find(".team_settings").click
+    find("#delete_team").click
+
+    expect(page).to have_current_path(root_path)
+  end
+end
